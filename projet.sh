ceb46cdd7cdf952c09a34f3c887f52e3ba5fa05c
Edouard <edouard@edouard-VirtualBox>
2014-05-17 16:38:33 +0700

    Modif config pour la connexion à la base données

--- projet.sh
+++ projet.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-#Mettre dans la variable l'URL du projet
-set ssh://53760dae4382ec8b61000111@projet-edouard.rhcloud.com/~/git/projet.git/
+
+#Mettre dans les variables : 
+#1.URL du projet 
+#2.Nom de la base de données 
+#3.User BDD
+#4.Password BDD
+set ssh://53760dae4382ec8b61000111@projet-edouard.rhcloud.com/~/git/projet.git/ projet adminQjM djffP
 
 #Ajout du site web 
 git init
@@ -12,6 +17,16 @@ rm index.php
 cd
 cp -r site/* projet
 cd projet
+
+#Modification des identifiants de la base de données
+echo '<?php
+define("DB_HOST", "localhost");
+define("DB_LOGIN", "'$2'");
+define("DB_PASS", "'$3'");
+define("DB_BDD", "'$1'");
+ ?>' > config.php
+
+#Commit
 git add -A :/
 git commit -m "Ajout du site"
 git push origin master
